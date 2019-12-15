2dda3ee177cdaa061a4ee6f8c8b3bfed88881e46
Rodrigo Valente <rodrigovalente@hotmail.com>
2019-12-15 09:13:33 -0300

    add controllers: LivroApagar, LivroEditar

--- controllers/livroctl.go
+++ controllers/livroctl.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
 	"strconv"
@@ -98,4 +99,71 @@ func (c ControllerLivro) LivroUnico(db *sql.DB) http.HandlerFunc {
 
 	}
 
+}
+
+//LivroApagar será exportado =========================================
+func (c ControllerLivro) LivroApagar(db *sql.DB) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		var error models.Error
+
+		if r.Method != "DELETE" {
+			http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+			return
+		}
+
+		// Params são os valores informados pelo usuario no URL
+		params := mux.Vars(r)
+		id, err := strconv.Atoi(params["id"])
+		if err != nil {
+			error.Message = "Numero ID inválido"
+		}
+
+		db.QueryRow("DELETE FROM livros where usuario_id=$1;", id)
+
+		SuccessMessage := "Livro deletado com sucesso!"
+
+		w.Header().Set("Content-Type", "application/json")
+		// w.Header().Set("Access-Control-Allow-Origin", "*")
+		utils.ResponseJSON(w, SuccessMessage)
+
+	}
+}
+
+//LivroEditar será exportado =========================================
+func (c ControllerLivro) LivroEditar(db *sql.DB) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		var livro models.Livro
+		var error models.Error
+
+		if r.Method != "PUT" {
+			http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+			return
+		}
+
+		params := mux.Vars(r)
+		id, err := strconv.Atoi(params["id"])
+		if err != nil {
+			error.Message = "Numero ID inválido"
+		}
+
+		json.NewDecoder(r.Body).Decode(&livro)
+
+		expressaoSQL := `UPDATE livros SET titulo=$1, titulo_original=$2, autor=$3, tradutor=$4, isbn=$5, cdd=$6, cdu=$7, ano=$8, tema=$9, editora=$10, paginas=$11, idioma=$12, formato=$13, dono=$14 WHERE livro_id=$15;`
+		_, err = db.Exec(expressaoSQL, livro.Titulo, livro.TituloOriginal, livro.Autor, livro.Tradutor, livro.Isbn, livro.Cdd, livro.Cdu, livro.Ano, livro.Tema, livro.Editora, livro.Paginas, livro.Idioma, livro.Formato, livro.Dono, id)
+		if err != nil {
+			panic(err)
+		}
+
+		row := db.QueryRow("select * from livros where livro_id=$1;", livro.ID)
+		err = row.Scan(&livro.ID, &livro.Titulo, &livro.TituloOriginal, &livro.Autor, &livro.Tradutor, &livro.Isbn, &livro.Cdd, &livro.Cdu, &livro.Ano, &livro.Tema, &livro.Editora, &livro.Paginas, &livro.Idioma, &livro.Formato, &livro.Dono)
+
+		w.Header().Set("Content-Type", "application/json")
+
+		utils.ResponseJSON(w, livro)
+
+	}
 }
--- main.go
+++ main.go
@@ -24,6 +24,8 @@ func main() {
 	// LIVRO URL ====================================
 	router.HandleFunc("/", livroctl.TodosLivros(db)).Methods("GET")
 	router.HandleFunc("/{id}", livroctl.LivroUnico(db)).Methods("GET")
+	router.HandleFunc("/deletar/{id}", livroctl.LivroApagar(db)).Methods("DELETE")
+	router.HandleFunc("/editar/{id}", livroctl.LivroEditar(db)).Methods("PUT")
 
 	log.Println("Listen on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", router))
