75cb2680370abc9679211e7c4a6d88b0c96bc2b8
Rodrigo Valente <rodrigovalente@hotmail.com>
2019-12-28 14:16:13 -0300

    add photurl on controllers

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 
 	// CORS ==========================================================
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:8080"},
+		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:8080", "http://localhost:8081"},
 		AllowCredentials: true,
 		// Enable Debugging for testing, consider disabling in production
 		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodDelete, http.MethodPut, http.MethodOptions},
